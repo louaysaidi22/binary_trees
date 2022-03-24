930e80ace06e11b8d0b41b8fbca3be4a7aa5a769
Aymenbs22 <aymenbenslima1@gmail.com>
2022-03-24 10:19:01 -0700

    function that checks if a binary tree is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height
+ * Return: the height of a binary tree
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+}
